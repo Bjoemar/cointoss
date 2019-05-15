e3ad3a1277e373336856deedc0af8ec372bb3873
bjoemar <bacleaanjoemar22@gmail.com>
2019-05-15 14:32:43 +0800

    fix the game

--- server.js
+++ server.js
@@ -110,12 +110,12 @@ function genRes(){
 						}
 
 						
-							var fs = require('fs');
-							let data = JSON.stringify(jsonObj);
+							// var fs = require('fs');
+							// let data = JSON.stringify(jsonObj);
 
-							setTimeout(function(){
-								fs.writerfileSync('result.json' , data);
-							},1000);
+							// setTimeout(function(){
+							// 	fs.writerfileSync('result.json' , data);
+							// },1000);
 
 					setTimeout(function(){
 						dbo.collection('game').insertOne(gameObj , function(eer , res){
